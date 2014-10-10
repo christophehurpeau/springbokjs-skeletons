866741c43031634d66884160e9ae0c0e08b1f930
Christophe Hurpeau <christophe@hurpeau.com>
2014-10-10 11:07:22 +0200

    update server

--- src/server/server.js
+++ src/server/server.js
@@ -1,15 +1,26 @@
 require('./init');
+var fs = require('fs');
 var express = require('express');
+var errorsParser = require('springbokjs-errors');
+var ErrorHtmlRenderer = require('springbokjs-errors/htmlRenderer');
+var errorHtmlRenderer = new ErrorHtmlRenderer();
 var app = express();
+
+var argv = require('minimist')(process.argv.slice(2));
+
+process.on('uncaughtException', function(err) {
+    try {
+        errorsParser.log(err);
+    } catch (err2) {
+        console.error(err2.stack);
+    }
+});
+
+
+logger.setPrefix('app: ', logger.blue.bold);
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
-
-var argv = require('minimist')(process.argv.slice(2), {
-    alias: {
-        'production': 'prod'
-    }
-});
 
 app.locals.basepath = argv.basepath || '/';
 
@@ -25,9 +36,35 @@ if (!argv.production) {
     console.log('Production mode');
 }
 
-app.get('/', (req, res) => res.render('index', { URL: req.path }));
+app.get('/', (req, res) => {
+    try {
+        res.render('index', { URL: req.path });
+    } catch(err) {
+        errorsParser.log(err);
+        res.statusCode = 500;
+        if (argv.production) {
+            res.send('Error: ' + err.message);
+        } else {
+            res.send(errorHtmlRenderer.render(err));
+        }
+    }
+});
 
 app.use(express.static(__dirname +'/../../public'));
 
+if (argv['socket-path']) {
+    try {
+        fs.unlinkSync(argv['socket-path']);
+    } catch(err) {
+    }
+}
+
 var port = argv.port || 3000;
-app.listen(port, console.log.bind(null, 'Listening on port ' + port));
+
+app.listen(argv['socket-path'] || port, function() {
+    if (argv['socket-path']) {
+        fs.chmodSync(argv['socket-path'], '777');
+    }
+    logger.log('Listening on '
+            +(argv['socket-path'] ? ' socket path ' + argv['socket-path'] : 'port ' + port));
+});
